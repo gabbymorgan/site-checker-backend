869b45d4b0558dcf6820b34912b328692f01b517
William Pelton <williamrpelton@gmail.com>
2018-06-13 22:25:47 -0500

    more adjustment for heroku

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ server.get('/scrape', (req, res) => {
       res.status(200).json({links, brokenImages, compatibilities});
     }).catch(err => {
       console.log(err);
-      res.status(500).json(err);
+      res.status(500).json({ message: 'Request is failing in the puppeteer function.'});
     });
   } else {
     res.status(300).json({message: "Requests must include a url."})
